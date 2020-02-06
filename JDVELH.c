a300fb2ff6d7578edaadbb8f78e9ab4b6929f7ef
BaptisteMorel <baptiste.morel82170@gmail.com>
2020-02-06 09:37:18 +0100

    Update JDVELH.c

--- JDVELH.c
+++ JDVELH.c
@@ -1,24 +1,58 @@
 #include <stdio.h>
+#include <string.h>
+
+// Declaration des structures
+struct Chapitre{
+    char description[400];
+    int gainOr;
+    int gainPDV;
+    int id[15];
+}; typedef struct Chapitre chapitre;
+
 
 // Declaration des variables globales
-int numChoix;
+int numChapitre;
+
+int deplacement(chapitre * adresse){  
+    int numChapitreD=-2;
+    int i;
+
+    printf("\n\n(-1) Quitter");
+
+    for(i=0; i<2; i++){
+        printf(" || (%d) ",i+1);
+        printf("chapitre %d",(*adresse).id[i]);
+    }
+    printf("\n");
+    
+    while(numChapitreD<-1 || numChapitreD>i){
+        scanf("%d",&numChapitreD);
+    }
+
+    return numChapitreD;
+}
 
 int main(){
     // Affectation des variables globales
-    numChoix=0;
+    numChapitre=0;
+
+    //Creation des structures
+    chapitre chapitres[15]={{"Aller a ZELANDIA",100,10,{1,2}},
+                    {"Aller a IRLANDIA",150,25,{0,2}},
+                    {"Aller a BRITANNIA",200,50,{0,1}}};
 
     // fonction
-    printf("Vous vous trouvez face a une fourchette dans la route.\nIl y a deux panneaux : ZELANDIA et IRLANDIA\n\n(1)Aller a ZELANDIA || (2)Aller a IRLANDIA");
     
-    while(numChoix!=1 && numChoix!=2){
-        scanf("%d",&numChoix); 
-    }
+    while(numChapitre!=-1){
+        printf("Chapitre %d : ",numChapitre);
+        printf("%s",chapitres[numChapitre].description);
 
-    if(numChoix==1){
-        printf("\nvous partez vers ZELANDIA");
-    }else {
-        printf("\nvous partez vers ZELANDIA");
+
+
+        numChapitre=deplacement(&chapitres[numChapitre]);
+
     }
+    
 
     return 0;
 }
