fb9da406e703dd075310fde2a104cce6b57ec6b3
BaptisteMorel <baptiste.morel82170@gmail.com>
2020-02-06 08:40:17 +0100

    Update JDVELH.c

--- JDVELH.c
+++ JDVELH.c
@@ -4,9 +4,21 @@
 int numChoix;
 
 int main(){
-    printf("Vous vous trouvez face a une fourchette dans la route. \n Il y a deux panneaux");
+    // Affectation des variables globales
+    numChoix=0;
 
+    // fonction
+    printf("Vous vous trouvez face a une fourchette dans la route.\nIl y a deux panneaux : ZELANDIA et IRLANDIA\n\n(1)Aller a ZELANDIA || (2)Aller a IRLANDIA");
+    
+    while(numChoix!=1 && numChoix!=2){
+        scanf("%d",&numChoix); 
+    }
 
+    if(numChoix==1){
+        printf("\nvous partez vers ZELANDIA");
+    }else {
+        printf("\nvous partez vers ZELANDIA");
+    }
 
     return 0;
 }
