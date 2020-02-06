19178f4483dfc0570660709a961417a8f2e26919
BaptisteMorel <baptiste.morel82170@gmail.com>
2020-02-06 08:33:36 +0100

    Update JDVELH.c

---
+++ JDVELH.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Declaration des variables globales
+int numChoix;
+
+int main(){
+    printf("Vous vous trouvez face a une fourchette dans la route. \n Il y a deux panneaux");
+
+
+
+    return 0;
+}
